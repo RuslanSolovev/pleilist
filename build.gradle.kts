f4bd7ae7a7737bd08c9d75e5f624ac41ac9f6943
RuslanSolovev <solovevrus1993@gmail.com>
2025-04-05 06:44:12 +0300

    Revert "комит"

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-@Suppress("DSL_SCOPE_VIOLATION") // Подавление предупреждений о DSL
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-    kotlin("kapt") version "1.9.0" apply false // Добавьте эту строку
 }
