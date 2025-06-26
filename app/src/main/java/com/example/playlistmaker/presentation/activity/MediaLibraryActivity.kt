0466d6f90ed2cb1b69e43ae2667963be2d7b411b
Руслан <66677799@mail.ru>
2025-06-26 15:51:42 +0300

    17

--- app/src/main/java/com/example/playlistmaker/presentation/activity/MediaLibraryActivity.kt
+++ app/src/main/java/com/example/playlistmaker/presentation/activity/MediaLibraryActivity.kt
@@ -2,13 +2,14 @@ package com.example.playlistmaker.presentation.ui.activity
 
 import MediaLibraryPagerAdapter
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.RelativeSizeSpan
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.ActivityMediaLibraryBinding
 import com.google.android.material.tabs.TabLayout
-
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MediaLibraryActivity : AppCompatActivity() {
@@ -25,14 +26,27 @@ class MediaLibraryActivity : AppCompatActivity() {
         val adapter = MediaLibraryPagerAdapter(this)
         viewPager.adapter = adapter
 
+        // Настройка TabLayoutMediator с форматированием текста
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-            tab.text = when (position) {
+            val tabText = when (position) {
                 0 -> getString(R.string.tab_favorites)
                 1 -> getString(R.string.tab_playlists)
                 else -> ""
             }
+
+
+            val spannableString = SpannableString(tabText)
+            spannableString.setSpan(
+                RelativeSizeSpan(1.3f),
+                0, 1,
+                android.text.Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+
+            // Устанавливаем отформатированный текст вкладки
+            tab.text = spannableString
         }.attach()
 
+        // Обработка кнопки "Назад"
         findViewById<ImageButton>(R.id.back_button).setOnClickListener { finish() }
     }
 }
